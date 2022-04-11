1f4eea213d7eb7e7edf8f0aa2435a5797c508a95
GonzaloHn <101721224+GonzaloHn@users.noreply.github.com>
2022-04-11 13:59:14 -0300

    Initial commit

---
+++ Assets/Scripts/Ejercicio.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio : MonoBehaviour
+{
+    public string nombre;
+    public int año;
+    public char orientación;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (nombre == "")
+        {
+            Debug.Log("Por favor, ingrese un nombre real.");
+        }
+        else if (año < 1 || año > 5)
+        {
+            Debug.Log("El año ingresado no es válido.");
+        }
+        else if(año == 1 || año == 2)
+        {
+            Debug.Log("Error. Aún estás en el ciclo básico.");
+        }
+        else if(orientación == 'T' || orientación == 'D' || orientación == 'G' || orientación == 'M' || orientación == 'H')
+        {
+            Debug.Log("Muchas gracias " + nombre + "!");
+        }
+        else
+        {
+            Debug.Log("“Solo puede ingresar T, D, G, M o H.");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
